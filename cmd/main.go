15fca31ae75158bd03c81a899251236025475573
Coldwws <mr.edil2004@mail.ru>
2026-05-24 14:35:34 +0500

    env

--- cmd/main.go
+++ cmd/main.go
@@ -8,9 +8,15 @@ import (
 	"github.com/Coldwws/306/internal/handler"
 	"github.com/Coldwws/306/internal/repository"
 	"github.com/Coldwws/306/internal/service"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	db, err := repository.NewPostgresDB()
 	if err != nil {
 		log.Fatal("Ошибка подключения к бд", err)
--- internal/repository/postgres.go
+++ internal/repository/postgres.go
@@ -2,21 +2,16 @@ package repository
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/jmoiron/sqlx"
+	_ "github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
 func NewPostgresDB() (*sqlx.DB, error) {
-	const (
-		host     = "localhost"
-		port     = 5432
-		user     = "postgres"
-		password = "Edil3875319"
-		dbname   = "306-database"
-	)
 	dsn := fmt.Sprintf(
-		"host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname,
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"),
 	)
 	db, err := sqlx.Connect("postgres", dsn)
 	if err != nil {
